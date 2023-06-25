ee5deec3c9c658da27b670d732596c70d27f1dc2
Fabriciooo <fabricerob@yahoo.fr>
2023-06-25 05:57:27 +0200

    commit

---
+++ src/app/components/Header.tsx
@@ -0,0 +1,43 @@
+"use client";
+import React from 'react';
+import { motion } from 'framer-motion';
+import { SocialIcon } from 'react-social-icons';
+
+
+type Props = {}
+
+export default function Header({}: Props) {
+  return (
+      <header className='sticky top-0 p-5 flex items-start justify-between max-w-7xl mx-auto z-20 xl:items-center'>
+      <motion.div
+        initial={
+          {
+            x: -500,
+            opacity: 0,
+            scale:0.5
+          }
+        }
+        animate={{
+          x: 0,
+          opacity: 1,
+          scale: 1
+        }}
+        transition={{
+          duration:1.5,
+        }}
+        className='flex flex-row items-center'>
+              {/* Social Icons */}
+              <SocialIcon url="https://github.com/FabriceROBERT" fgColor='gray' bgColor='transparent'/>
+              <SocialIcon url="https://www.linkedin.com/in/fabrice-robert9536/"fgColor='gray' bgColor='transparent' />
+              <SocialIcon url="https://www.linkedin.com/in/fabrice-robert9536/"fgColor='gray' bgColor='transparent' />
+          </motion.div>
+
+
+          <div className='flex flex-row items-center text-gray-300 cursor-pointer '>
+          <SocialIcon fgColor='gray' network='email' bgColor='transparent' />
+          <p className="uppercase hidden md:inline-flex text-sm text-gray-400"> Prendre Contact</p>
+          </div>
+         
+        </header>
+  )
+}
