984d2b06ce7fe0e2138e894526055522e4f4d7ea
Fabriciooo <fabricerob@yahoo.fr>
2023-06-26 01:54:53 +0200

    commit

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -29,14 +29,28 @@ export default function Header({}: Props) {
               {/* Social Icons */}
               <SocialIcon url="https://github.com/FabriceROBERT" fgColor='gray' bgColor='transparent'/>
               <SocialIcon url="https://www.linkedin.com/in/fabrice-robert9536/"fgColor='gray' bgColor='transparent' />
-              <SocialIcon url="https://www.linkedin.com/in/fabrice-robert9536/"fgColor='gray' bgColor='transparent' />
           </motion.div>
 
 
-          <div className='flex flex-row items-center text-gray-300 cursor-pointer '>
+          <motion.div initial={
+          {
+            x: 500,
+            opacity: 0,
+            scale:0.5,
+          }
+        }  
+        animate={{
+          x: 0,
+          opacity: 1,
+          scale: 1
+        }}
+        transition={{
+          duration:1.5,
+        }}
+        className='flex flex-row items-center text-gray-300 cursor-pointer '>
           <SocialIcon fgColor='gray' network='email' bgColor='transparent' />
           <p className="uppercase hidden md:inline-flex text-sm text-gray-400"> Prendre Contact</p>
-          </div>
+          </motion.div>
          
         </header>
   )
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,23 @@
+import Head from "next/head";
 import Header from "./components/Header";
 import React from "react";
+import Hero from "./components/Hero";
 
 
 
 export default function Home() {
   return (
-    <div> 
-    
+    <div className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-scroll z-0"> 
+    <title>ROBERT Fabrice Portfolio</title>
+     
       {/* Header  */}
 
-      <Header/>
-
+      <Header />
+      
+      {/* Hero */}
+      <section id="hero" className="snap-center">
+        <Hero/>
+      </section>
     
     {/* // About */}
 
